707134e459404fc02dfb89f63b788bc8cc22a7ff
kai <azrailkai@gmail.com>
2018-03-25 23:13:40 +0600

    Add burgerBuilder container and Burger, BurgerIngredients component add for theese styles

---
+++ src/components/Burger/BurgerIngridient/BurgerIngredient.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styles from './BurgerIngredient.css';
+
+class BurgerIngredient extends  Component {
+    render () {
+        let ingridient = null;
+
+        switch(this.props.type) {
+            case ('bread-bottom'):
+                ingridient = <div className={styles.BreadBottom}></div>
+                break;
+            case ('bread-top'):
+                ingridient = (
+                    <div className={styles.BreadTop}>
+                        <div className={styles.Seeds1}></div>
+                        <div className={styles.Seeds2}></div>
+                    </div>
+                );
+                break;
+            case ('meat'):
+                ingridient = <div className={styles.Meat}></div>
+                break;
+            case ('cheese'):
+                ingridient = <div className={styles.Cheese}></div>
+                break;
+            case ('salad'):
+                ingridient = <div className={styles.Salad}></div>
+                break;
+            case ('bacon'):
+                ingridient = <div className={styles.Bacon}></div>
+                break;
+            default:
+                ingridient = null;
+        }
+
+        return ingridient;
+    }
+}
+
+BurgerIngredient.propTypes = {
+  type: PropTypes.string.isRequired
+};
+
+export default BurgerIngredient;
